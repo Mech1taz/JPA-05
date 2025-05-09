1c47376b9d17a7da6c743559b65ac314fc6f217a
MechitaZ <ma.astudillom@duocuc.cl>
2025-05-09 11:05:24 -0400

    arreglos en controller

--- orm4/src/main/java/com/mechitaz/orm4/controller/PacienteController.java
+++ orm4/src/main/java/com/mechitaz/orm4/controller/PacienteController.java
@@ -1,11 +1,34 @@
 package com.mechitaz.orm4.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mechitaz.orm4.model.Paciente;
+import com.mechitaz.orm4.service.PacienteService;
+
 
 @RestController
-@RequestMapping("api/v1/pacientes")
+@RequestMapping("/api/v1/pacientes")
 public class PacienteController {
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> getPacientes() {
+        List<Paciente> pacientes = pacienteService.findAll();
+        if (!pacientes.isEmpty()) {
+            return new ResponseEntity<>(pacientes, HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
 }
